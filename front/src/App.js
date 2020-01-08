1bdd14b8d87481bcb180db0f30bdcfbc383975d1
Louis Dupas <louisdupas@net-c.com>
2020-01-08 11:11:38 +0100

    modif composant Fonctionnement

--- front/src/App.js
+++ front/src/App.js
@@ -2,17 +2,8 @@ import React from 'react';
 import './App.css';
 import { BrowserRouter as Router, Switch, Route } from "react-router-dom";
 import Accueil from './components/accueil/Accueil';
+import Fonctionnement from './components/fonctionnement/Fonctionnement';
 import Contact from './components/contact/Contact';
-import Navigation from './components/navigation/Navigation';
-
-
-import Carousel from './components/accueil/carousel/Carousel';
-import Horaires from './components/accueil/horaires/Horaires';
-import Compteurs from './components/accueil/compteurs/Compteurs';
-
-import Explication from './components/fonctionnement/explication/Explication';
-import Objets from './components/fonctionnement/objets/Objets';
-import Plan from './components/fonctionnement/plan/Plan';
 
 function App() {
   return (
@@ -20,6 +11,7 @@ function App() {
       <Router>
         <Switch>
           <Route exact path='/' exact component={Accueil} />
+          <Route path='/fonctionnement' component={Fonctionnement} />
           <Route path='/contact' component={Contact} />
         </Switch>
       </Router>
@@ -27,15 +19,5 @@ function App() {
     
   )
 } 
-
-
-{/*         <Carousel />
-        <Horaires />
-        <Compteurs /> */}
-
-
-{/*         <Explication />
-        <Objets />
-        <Plan /> */}
 
 export default App;
